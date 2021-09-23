410fff5e1713791205e8f33dd7a833441646bcad
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-09-24 01:24:23 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I just got clicked!");
+    var audio = new Audio('sounds/tom-1.mp3');
+  audio.play();
   });
+
 }
